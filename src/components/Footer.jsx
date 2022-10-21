30b6b919d897dd1a75a6157e7e767d8ef61fe2d9
Susanna Cho <susannacho@Susannas-MacBook-Pro.local>
2022-10-22 09:29:55 +1100

    text are centered, have a modal but no form yet

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
-const { Container, Text } = require("@chakra-ui/react")
+const { Container, Text, Box } = require("@chakra-ui/react")
 
 const Footer = () => {
     return (
-        <Container sx={{borderTop: "2px solid black" }}>
+        <Container sx={{borderTop: "2px solid black", textAlign: "center"}}>
+            <Box>
             <Text>Made with 🥦 in Melbourne</Text>
             <Text>© 2022 Brocolli & Co. All rights reserved.</Text>
+            </Box>
         </Container>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,53 @@
-const { Container, Text, Heading, Button, Center } = require("@chakra-ui/react")
+const { Container, Text, Heading, Button, 
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+  
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,} = require("@chakra-ui/react")
+
 
 const Home = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+
     return (
-        <Container>
+        <Container sx={{display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center"}}>
             <Heading>A better way <br></br>
             to enjoy every day.</Heading>
             <Text>Be the first to know when we launch</Text>
-            <Button>Request an invite</Button>
+            <Button onClick={onOpen}>Request an invite</Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Request an invite</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* form */}
+            <FormControl>
+                <FormLabel>First Name</FormLabel>
+                <input type='text' />
+                <FormLabel>Email address</FormLabel>
+                <input type='email' />
+                <FormLabel>Confirm Email address</FormLabel>
+                <input type='email' />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Send
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
         </Container>
     )
 }
