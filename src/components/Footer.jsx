4ae592e6bb0e5f73d5bc1651f2e34984d3ca4d44
Susanna Cho <susannacho@Susannas-MBP.gateway>
2022-10-22 15:23:00 +1100

    header and footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const { Text, Box } = require("@chakra-ui/react");
 
 const Footer = () => {
   return (
-    <Box sx={{ borderTop: "2px solid black", textAlign: "center" }}>
+    <Box sx={{ borderTop: "2px solid black", textAlign: "center", py: 3 }}>
       <Box>
         <Text>Made with 🥦 in Melbourne</Text>
         <Text>© 2022 Brocolli & Co. All rights reserved.</Text>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@ const { Text, Box } = require("@chakra-ui/react");
 
 const Header = () => {
   return (
-    <Box>
-      <Text sx={{ borderBottom: "2px solid black" }}>Brocolli & Co</Text>
+    <Box sx={{ py: 5, borderBottom: "2px solid black" }}>
+      <Box sx={{ ml: 6 }}>
+        <Text sx={{ textTransform: "uppercase" }}>Brocolli & Co</Text>
+      </Box>
     </Box>
   );
 };
